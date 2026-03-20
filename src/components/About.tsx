8529a93235efa9e5ca8746aff42289fe5ecbd586
Shaurya Bhatt <shauryabhatt333@gmail.com>
2026-03-20 11:32:20 +0530

    Add files via upload

---
+++ src/components/About.tsx
@@ -0,0 +1,17 @@
+import "./styles/About.css";
+
+const About = () => {
+  return (
+    <div className="about-section" id="about">
+      <div className="about-me">
+        <h3 className="title">About Me</h3>
+        <p className="para">
+          Aspiring AI/ML Enginner.Skilled in Python(Advanced),C++(Advanced),Java(Intermediate)
+          
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default About;
